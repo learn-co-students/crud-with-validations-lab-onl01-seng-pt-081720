263a5f16cf55e24b2f39c0c92aa3766c543078e6
jasonemarkson <jasonmarkson76@gmail.com>
2021-01-08 20:00:00 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,9 +2,20 @@ class Song < ActiveRecord::Base
     validates :title, presence: true
     validates :title, uniqueness: true
     validates :release_year, presence: false, if: :released == false
-    #validates :release_year == Time.current.year
+    validate :current_year
+    validate :released?
 
     def current_year
-        Time.current.year
+        if release_year
+            if self.release_year > Time.current.year
+                errors.add(:release_year, "Release year is in the future")
+            end
+        end
+    end
+
+    def released?
+        if released == true && !release_year
+            errors.add(:released, "This song is released but missing a release year")
+        end
     end
 end
