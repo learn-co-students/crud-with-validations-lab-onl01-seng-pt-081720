30248e72a21d4bb68d5a4d0ec1dacc8ffe40ad7c
jasonemarkson <jasonmarkson76@gmail.com>
2021-01-08 19:37:50 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,15 +10,33 @@ class SongsController < ApplicationController
     def create
         @song = Song.new(title: params[:song][:title], released: params[:song][:released], release_year: params[:song][:release_year], artist_name: params[:song][:artist_name], genre: params[:song][:genre])
         
-        @song.save
-        redirect_to song_path(@song)
+        if @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
     end
 
     def show
         @song = Song.find(params[:id])
     end
-    
+
     def edit
         @song = Song.find(params[:id])   
+    end
+    
+    def update
+        @song = Song.find(params[:id])
+        
+        if @song.update(params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
+            redirect_to song_path(@song)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @song = Song.find(params[:id]).destroy
+        redirect_to songs_path
     end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,9 @@ class Song < ActiveRecord::Base
     validates :title, presence: true
     validates :title, uniqueness: true
     validates :release_year, presence: false, if: :released == false
+    #validates :release_year == Time.current.year
+
+    def current_year
+        Time.current.year
+    end
 end
